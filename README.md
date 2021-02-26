4e1ebf0d44bd9cc766a90c718623072c68738fb9
Parth Shah <parth.teaching@gmail.com>
2021-02-26 15:35:03 -0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# mysoundvisualizer
-my first coding project!
+# p5starter
+
+Download this starter code!
+
+
+
+Set up a server: https://chrome.google.com/webstore/detail/web-server-for-chrome/ofhbbkphhbklhfoeikjpcbhemlocgigb/
+
+Set the folder to the starter code.
---
+++ sketch.js
@@ -0,0 +1,34 @@
+let song, amplitude;
+
+function preload() {
+  song = loadSound('music.mp3');
+}
+
+
+
+function setup() {
+  createCanvas(displayWidth, displayHeight);
+  amplitude = new p5.Amplitude();
+
+}
+
+function draw() {
+  background(2);
+  let level = amplitude.getLevel();
+  let diam = map(level, 0, 1, 10, 400);
+  fill(255, 0, 0);
+  ellipse(displayWidth/2,displayHeight/2,diam,diam);
+ 
+}
+
+
+function mousePressed(){
+	if (song.isPlaying()) {
+		song.stop()
+	} else {
+		song.play()
+	}
+}
+
+
+
